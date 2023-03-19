890677a8c70e2f252dc61e05af8e40f7d974ded0
Kevin Nipal <kmn6@njit.edu>
2023-03-18 21:02:34 -0400

    add footer links check

---
+++ tests/footer_links.spec.js
@@ -0,0 +1,60 @@
+const { test, expect } = require('@playwright/test')
+
+const pagesToCheck = ['/',
+                      '/contact.html',
+                      '/templates/template-content.html',
+]
+
+test('test-valid-footer-links', async ({browser}) => {
+    const context = await browser.newContext()
+    const page = await context.newPage()
+
+    for (const pageToCheck of pagesToCheck) {
+        // console.log("Checking page " + pageToCheck)
+
+        await page.goto(pageToCheck)
+        const links = await page.$$("footer a")
+
+        for (const link of links) {
+            const url = await link.getAttribute('href')
+            console.log("Checking " + pageToCheck + " for valid footer link to " + url)
+            if (url.startsWith('#')) {
+                console.log(`Skipping link "${url}" because it is not a new page.`)
+                continue
+            }
+
+            const newPage = await context.newPage()
+            const response = await newPage.goto(url)
+            const status = response.status()
+            expect(response.ok()).toBe(true)
+
+            await newPage.close()
+        }
+    }
+})
+
+test('footer-contains-correct-links-and-wording', async ({page}) => {
+    // 2d array of [words,link]
+    const expectedNavbarLinks = [ ["Privacy Policy", "/privacy.html"],
+                                  ["Contact","/contact.html"]
+    ]
+
+    for (const pageToCheck of pagesToCheck) {
+        // console.log("Checking page " + pageToCheck)
+
+        await page.goto(pageToCheck)
+        const links = await page.$$("footer a")
+
+        for (let i = 0; i < links.length; i++) {
+            const link = links[i]
+            const textContent = await link.textContent()
+            const linkedUrl = await link.getAttribute("href")
+
+            console.log("Checking " + pageToCheck + " has " + expectedNavbarLinks[i][0] + " linking to " + expectedNavbarLinks[i][1])
+            expect (textContent).toBe(expectedNavbarLinks[i][0])
+            expect (linkedUrl).toBe(expectedNavbarLinks[i][1])
+
+        }
+
+    }
+})
