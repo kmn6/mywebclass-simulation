50455270a4ba24581ff64ec07f2cb0ddbe1f95cf
Kevin Nipal <kmn6@njit.edu>
2023-03-19 15:48:50 -0400

    debug modal test

--- tests/privacy_modal.spec.js
+++ tests/privacy_modal.spec.js
@@ -18,21 +18,23 @@ test('Privacy Modal Test', async ({ browser }) => {
   const modalContent = await modal.$eval('.modal-body', el => el.textContent);
   expect(modalContent).toContain('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
 
+  // Click the Decline button and check that the page redirects to Google then go back
+  page.locator('.modal-content #declineBtn').click()
+  await page.waitForNavigation()
+  expect(page.url()).toBe('https://www.google.com/');
+  await page.goBack();
+
   // Click the Accept button and check that the modal closes and sets the cookie
-  await Promise.all([
-    page.waitForNavigation(),
-    modal.click('#acceptBtn')
-  ]);
+  await page.locator('.modal-content #acceptBtn').click()
+  await page.waitForSelector('body.modal-open', { state: 'detached' });
   expect(await modal.isVisible()).toBeFalsy();
-  expect(await context.cookies()).toContainEqual({ name: 'privacyAccepted', value: 'true' });
+  const cookies = await context.cookies();
+  const privacyAccepted = cookies.find(cookie => cookie.name === 'privacyAccepted');
+  expect(privacyAccepted.value).toEqual('true');
 
   // Navigate back to the page and check that the modal does not appear
-  await page.goto('http://localhost:3000');
+  await page.reload( {waitUntil: 'domcontentloaded' });
   expect(await modal.isVisible()).toBeFalsy();
-
-  // Click the Decline button and check that the page redirects to Google
-  await modal.click('#declineBtn');
-  expect(page.url()).toBe('https://www.google.com/');
 
   // Close the context
   await context.close();
