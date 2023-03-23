6d319fd3fd93dd5448b893fac2c11a4250eddd0e
Kevin Nipal <kmn6@njit.edu>
2023-03-22 22:04:43 -0400

    add privacy.html to the other tests

--- tests/seo-meta.spec.js
+++ tests/seo-meta.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-const pages = ['index.html', 'template-content.html', 'contact.html'];
+const pages = ['index.html', 'template-content.html', 'contact.html','privacy.html'];
 
 for (const pageUrl of pages) {
     test(`Verify SEO meta tags - ${pageUrl}`, async ({ page }) => {
